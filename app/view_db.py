7351e5538a686dde87dad7bd84671ed9de265e43
Maaz Imam <maazimam03@gmal.com>
2024-11-02 13:50:55 +0500

    Updated readme.md and updated view_db.py for better representation of database

--- app/view_db.py
+++ app/view_db.py
@@ -2,11 +2,15 @@ from app_init import app, db
 from models import Item, Shelf
 
 with app.app_context():
-    print('here')
+    print(f"{'Item ID':<10} {'Name':<30} {'Price':<10} {'Shelf ID':<10}")
+    print("-" * 60)
     items = Item.query.all()
     for item in items:
-        print(item.item_id, item.name, item.price, item.shelf_id)
+        print(f"{item.item_id:<10} {item.name:<30} {item.price:<10} {item.shelf_id:<10}")
 
-    shelves= Shelf.query.all()
+    print("\n")
+    print(f"{'Shelf ID':<10} {'Manager':<30} {'Location':<30}")
+    print("-" * 70)
+    shelves = Shelf.query.all()
     for shelf in shelves:
-        print(shelf.shelf_id, shelf.manager, shelf.location)
+        print(f"{shelf.shelf_id:<10} {shelf.manager:<30} {shelf.location:<30}")
